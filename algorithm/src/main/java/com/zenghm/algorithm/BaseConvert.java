70d8bb2f0e94c53219bf3272b867440fb9dd3556
airlen <zenghm@fingard.com>
2021-03-07 22:01:20 +0800

    十进制转二进制

---
+++ algorithm/src/main/java/com/zenghm/algorithm/BaseConvert.java
@@ -0,0 +1,32 @@
+package com.zenghm.algorithm;
+
+import java.util.ArrayList;
+
+/**
+ * @author Airlen
+ * @date 2021/3/7
+ * @description xxx
+ */
+public class BaseConvert {
+    public String convert(int num,int base){
+        ArrayList<Integer> baseNumber = new ArrayList<>();
+        while (true){
+            baseNumber.add(num%base);
+            num = num/base;
+            if(num==1){
+                break;
+            }
+        }
+        baseNumber.add(1);
+        StringBuilder result = new StringBuilder(baseNumber.size());
+        for (int index = baseNumber.size()-1;index>=0;index--){
+            result.append(baseNumber.get(index));
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        BaseConvert baseConvert = new BaseConvert();
+        System.out.println(baseConvert.convert(22,2));
+    }
+}
---
+++ jvm/src/main/java/com/zenghm/jvm/Application.java
@@ -0,0 +1,19 @@
+package com.zenghm.jvm;
+
+/**
+ * @author Airlen
+ * @date 2021/3/6
+ * 关于jvm 的相关学习
+ *   /**
+ *          * jmv 虚拟机总共分为几个重要的知识点
+ *          * 1、Class类文件结构
+ *          * 2、运行时数据区
+ *          * 3、字节码执行引擎
+ *          * 4、垃圾回收GC
+ *          * 5、jvm调优实战
+ *          */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}

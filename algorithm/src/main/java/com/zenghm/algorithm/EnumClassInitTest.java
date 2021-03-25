978e3e0b0217bee7244c854d6751a100f30d3872
airlen <zenghm@fingard.com>
2021-03-25 15:51:09 +0800

    枚举值初始化先后顺序

---
+++ algorithm/src/main/java/com/zenghm/algorithm/EnumClassInitTest.java
@@ -0,0 +1,34 @@
+package com.zenghm.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @author Airlen
+ * @date 2021/3/25
+ * @description 枚举类的初始化的过程测试
+ * 测试结果：枚举值 > 静态变量 > 静态代码块
+ */
+public enum  EnumClassInitTest {
+    A(1),
+    B(2),
+    C(3);
+    EnumClassInitTest(Integer val){
+        System.out.println("初始化："+val);
+        this.value= val;
+    }
+    private Integer value;
+    private static Inner inner = new Inner();
+    static class Inner{
+        Inner(){
+            System.out.println("Inner初始化");
+        }
+    }
+    static {
+        System.out.println("static 代码块执行");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(EnumClassInitTest.class);
+        Arrays.stream(EnumClassInitTest.values()).forEach(System.out::println);
+    }
+}
